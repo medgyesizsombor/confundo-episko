11b27743cd66edb7f790e8713d52fc4bbbb7a3e4
Medgyesi Zsombor <medgyesizs123@gmail.com>
2021-06-23 18:42:58 +0200

    make css better

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -15,20 +16,27 @@ export class LoginPage implements OnInit {
     password: ''
   }
 
-  constructor(private angularFirestore: AngularFirestore, private angularFireAuth: AngularFireAuth, private router: Router) { }
+  constructor(private angularFirestore: AngularFirestore, private angularFireAuth: AngularFireAuth, private router: Router, private loadingController: LoadingController) { }
 
   ngOnInit() {
     
   }
 
   async login(){
+    let loading = await this.loadingController.create({
+      message: 'Loading...'
+    });
+    loading.present();
+
     const user = await this.angularFireAuth.signInWithEmailAndPassword(this.user.email, this.user.password);
     console.log(user);
 
     if(user.user.email){
       this.router.navigate(['/home']);
+      loading.dismiss();
     } else {
       alert('login failed!');
+      loading.dismiss();
     }
   }
 
@@ -39,7 +47,7 @@ export class LoginPage implements OnInit {
     if(user.user.email){
       alert('registration successful!');
     } else {
-      alert('registration failed!'); //no internet connection, email is already used, email is not in good format
+      alert('registration failed!'); //no internet connection, email is already used, email is not properly formatted
     }
   }
   
