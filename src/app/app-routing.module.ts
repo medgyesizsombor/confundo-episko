c2cd7a7678371d3becc0a3fea752e11d8c02711c
Medgyesi Zsombor <medgyesizs123@gmail.com>
2021-07-01 18:35:08 +0200

    created settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'games',
+    redirectTo: 'settings',
     pathMatch: 'full'
   },
   {
