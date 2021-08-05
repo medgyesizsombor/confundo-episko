18f744d3b40518b0733b815c825aa4d49a2f373f
Medgyesi Zsombor <medgyesizs123@gmail.com>
2021-08-05 18:34:40 +0200

    router settings

--- src/app/games/games.page.ts
+++ src/app/games/games.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-games',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToGame(){
+    this.router.navigate(['main-tabs/colourgame']);
   }
 
 }
