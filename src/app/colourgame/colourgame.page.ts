95e0b1a5ba4e281da012db7080821c0b1d546229
Medgyesi Zsombor <medgyesizs123@gmail.com>
2021-08-25 14:58:31 +0200

    asd

--- src/app/colourgame/colourgame.page.ts
+++ src/app/colourgame/colourgame.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -19,8 +21,12 @@ export class ColourgamePage implements OnInit {
 
   playing: boolean = false;
   ended: boolean = false;
+
+  points = {
+    finalPoint: ''
+  }
   
-  constructor(private router: Router, private route: ActivatedRoute) {
+  constructor(private router: Router, private route: ActivatedRoute, private angularFireStore: AngularFirestore, private angularFireAuth: AngularFireAuth) {
   }
 
   ngOnInit() {
@@ -95,6 +101,9 @@ export class ColourgamePage implements OnInit {
     this.finalResult = "You have got " + this.result + " points!"
     this.playing = false;
     this.ended = true;
+    /*this.angularFireStore.collection('Users/${user}/points').add('points').then(() => {
+      finalResult: this.finalResult;
+    })*/
   }
 
 }
