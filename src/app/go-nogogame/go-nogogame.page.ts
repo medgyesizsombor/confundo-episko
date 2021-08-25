3ad5c26374e85523383bc332169cc473001c8293
Medgyesi Zsombor <medgyesizs123@gmail.com>
2021-08-25 16:06:39 +0200

    created go-nogo page

---
+++ src/app/go-nogogame/go-nogogame.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-go-nogogame',
+  templateUrl: './go-nogogame.page.html',
+  styleUrls: ['./go-nogogame.page.scss'],
+})
+export class GoNogogamePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
