d56ead0c563dc76815ac6dc696e4386c834d6735
Medgyesi Zsombor <medgyesizs123@gmail.com>
2021-08-25 16:42:31 +0200

    started to work on go-nogo game

--- src/app/go-nogogame/go-nogogame.page.ts
+++ src/app/go-nogogame/go-nogogame.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-go-nogogame',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoNogogamePage implements OnInit {
 
-  constructor() { }
+  text: number;
+  counter: number = 0;
+
+  playing: boolean = false;
+  nextTask: boolean = false;
+
+  constructor(private angularFireStore: AngularFirestore, private angularFireAuth: AngularFireAuth) { }
 
   ngOnInit() {
+    this.text = Math.floor(Math.random() * 10 - 0) + 0;
+  }
+
+  start(){
+    this.fillNumber();
+    this.firstTask();
+  }
+
+  fillNumber(){
+    this.text = Math.floor(Math.random() * 10 - 0) + 0;
+  }
+
+  firstTask(){
+    if((this.text / 2) == 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  secondTask(){
+    if((this.text % 3) == 0){
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
