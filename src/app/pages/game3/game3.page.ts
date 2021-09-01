85f931188e104fe4eff23512fbc369a7c02d7a81
Medgyesi Zsombor <medgyesizs123@gmail.com>
2021-09-01 14:08:04 +0200

    some changes on game3

--- src/app/pages/game3/game3.page.ts
+++ src/app/pages/game3/game3.page.ts
@@ -7,9 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Game3Page implements OnInit {
 
-  constructor() { }
+  lbl1text: number;
+  lbl2text: number;
+  lbl3text: number;
+  lbl4text: number;
+  lbl5text: number;
+  lbl6text: number;
+  lbl7text: number;
+  turn = 0;
+  numberArray = [1, 2, 3];
 
-  ngOnInit() {
+  playing = false;
+
+  constructor(){ }
+
+  ngOnInit(){
   }
+
+  start(){
+    this.playing = true;
+  }
+
+  rightButton($event) {
+    this.changeNumber();
+  }
+
+  changeNumber(){
+    this.turnCount();
+    for(let i = 0; i < this.numberArray.length; i++){
+      this.numberArray[i] = Math.floor(Math.random() * (20 - 0) + 0);
+    }
+    console.log(this.numberArray);
+    if ((this.numberArray.length < 7) && (this.turn % 3 === 0)) {
+      this.numberArray.length = this.numberArray.length + 1;
+    }
+    this.numberArray[this.numberArray.length - 1] = Math.floor(Math.random() * (20 - 0) + 0);
+    console.log(this.numberArray.length);
+    console.log('----');
+
+    this.lbl1text = this.numberArray[0];
+    this.lbl2text = this.numberArray[1];
+    this.lbl3text = this.numberArray[2];
+    this.lbl4text = this.numberArray[3];
+    this.lbl5text = this.numberArray[4];
+    this.lbl6text = this.numberArray[5];
+    this.lbl7text = this.numberArray[6];
+  }
+
+  endOfTurn(){
+
+  }
+
+  turnCount(){
+    this.turn++;
+    console.log('turn: ' + this.turn);
+  }
+
+
+
 
 }
