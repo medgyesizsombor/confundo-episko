a78e830116833ba8b100b04ccd8b572ac7c3345a
Medgyesi Zsombor <medgyesizs123@gmail.com>
2021-11-01 18:39:05 +0100

    asd

--- src/app/pages/game3/game3.page.ts
+++ src/app/pages/game3/game3.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import * as moment from 'moment';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { DataAverageUserService } from 'src/app/services/data-average-user/data-average-user.service';
 import { DataOfGameService } from 'src/app/services/data-of-game/data-of-game.service';
+import { DataOfUserService } from 'src/app/services/data-of-user/data-of-user.service';
 
 @Component({
   selector: 'app-game3',
@@ -28,6 +31,7 @@ export class Game3Page implements OnInit {
   clickCounter = 0;
   result = 0;
 
+
   turn = 0;
   numberArray = [0, 0, 0];
   timeText = '';
@@ -43,10 +47,19 @@ export class Game3Page implements OnInit {
   bestScore: number;
 
   uid = localStorage.getItem('uid');
+  average = '';
 
+  userBirthdate: any;
+  userAge: number;
+  today = moment(Date.now());
+
+  playedGamesAverage = 0;
+  sumScoreAverage = 0;
+  averageScoreAverage = 0;
 
   constructor(private angularFireStore: AngularFirestore, private angularFireAuth: AngularFireAuth,
-    private authService: AuthService, private dataOfGame: DataOfGameService){ }
+    private authService: AuthService, private dataOfGame: DataOfGameService,
+    private dataOfUser: DataOfUserService, private dataAverageUser: DataAverageUserService){ }
 
   ngOnInit(){
   }
@@ -239,6 +252,87 @@ export class Game3Page implements OnInit {
     });
   }
 
+  async getDataOfAverageUser(){
+    await this.dataAverageUser.getDataOfAverageUser('thirdgame').then(() => {
+      this.playedGamesAverage = Number(localStorage.getItem('playedGamesAverage'))+1;
+      this.sumScoreAverage = Number(localStorage.getItem('sumScoreAverage'))+this.result;
+      this.averageScoreAverage = this.sumScoreAverage / this.playedGamesAverage;
+    });
+  }
+
+  async getDataOfUser(){
+    await this.dataOfUser.getDataOfUser().then(() => {
+      this.userBirthdate = localStorage.getItem('birthdate');
+    });
+
+    const diff = moment.duration(this.today.diff(this.userBirthdate));
+    const age = Number(diff.years());
+    if(age >= 0 && age <= 4){
+      this.average = '0-4';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 5 && age <= 9){
+      this.average = '5-9';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 10 && age <= 14){
+      this.average = '10-14';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 15 && age <= 19){
+      this.average = '15-19';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 20 && age <= 24){
+      this.average = '20-24';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 25 && age <= 29){
+      this.average = '25-29';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 30 && age <= 34){
+      this.average = '30-34';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 35 && age <= 39){
+      this.average = '35-39';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 40 && age <= 44){
+      this.average = '40-44';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 45 && age <= 49){
+      this.average = '45-49';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 50 && age <= 54){
+      this.average = '50-54';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 55 && age <= 59){
+      this.average = '55-54';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 60 && age <= 64){
+      this.average = '60-64';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 65 && age <= 69){
+      this.average = '65-69';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 70 && age <= 74){
+      this.average = '70-74';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 75 && age <= 79){
+      this.average = '75-79';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 80 && age <= 84){
+      this.average = '80-84';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 85 && age <= 89){
+      this.average = '85-89';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 90 && age <= 94){
+      this.average = '90-94';
+      localStorage.setItem('averageId', this.average);
+    } else if (age >= 95 && age <= 99){
+      this.average = '95-99';
+      localStorage.setItem('averageId', this.average);
+    } else {
+      this.average = '100+';
+      localStorage.setItem('averageId', this.average);
+    }
+  }
+
   async end(){
     this.ended = true;
     this.finalResult = 'You have got ' + this.result + ' points!';
@@ -249,6 +343,14 @@ export class Game3Page implements OnInit {
       sumScore: this.sumScore,
       bestScore: this.bestScore,
       averageScore: this.averageScore
+    });
+
+    await this.getDataOfUser();
+    await this.getDataOfAverageUser();
+    this.angularFireStore.collection('Statistics').doc(this.average).collection('game').doc('thirdgame').update({
+      playedGames: this.playedGamesAverage,
+      sumScore: this.sumScoreAverage,
+      averageScore: this.averageScoreAverage,
     });
 
     clearInterval(this.interval);
